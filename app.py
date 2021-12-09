7ea128fac9e1dc5919fd31dfde0b0624ffa5ec90
worth-winchester <worthhw@gmail.com>
2021-12-09 17:28:05 +0000

    12/9/21 12:27PM

--- app.py
+++ app.py
@@ -1,25 +1,60 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index2.html")
+    return render_template("index.html")
 
-@app.route("/movie", methods=['GET', 'POST'])
-def movie():
-    title = request.args.get("title")
-    if title != None:
-        requestUrl = "http://www.omdbapi.com/?apikey=a514574c&t=" + title
-        apiResponse = requests.get(requestUrl)
-        responseJson = apiResponse.json()
-        responseText = apiResponse.text
-        with open("test.txt", "w") as f:
-            f.write(responseText)
-    #return redirect("/", movieData=movieJson)
-    return render_template("index2.html", movieData=responseJson)  
+@app.route("/movieTitle")
+def movieTitle():
+    return render_template("searchByTitle.html")
+
+@app.route("/movieID")
+def movieID():
+    return render_template("searchByID.html")
+
+# @app.route("/movieTitle", methods=['GET', 'POST'])
+# def getTitle():
+#     if request.method == 'POST':
+#         if request.form["movieTitle"] != "":
+#             title = request.form["movieTitle"]
+#             requestUrl = "http://www.omdbapi.com/?apikey=a514574c&t=" + title
+#             apiResponse = requests.get(requestUrl)
+#             responseJson = apiResponse.json()
+#             responseText = apiResponse.text
+#             with open("test.txt", "w") as f:
+#                 f.write(responseText)
+#             return render_template("searchByTitle.html", movieData=responseJson)
+
+@app.route("/movieTitle", methods=['GET', 'POST'])
+def getTitle():
+    if request.method == 'POST':
+        if request.form["movieTitle"] != "":
+            title = request.form["movieTitle"]
+            requestUrl = "http://www.omdbapi.com/?apikey=a514574c&t=" + title
+            apiResponse = requests.get(requestUrl)
+            responseJson = apiResponse.json()
+            responseText = apiResponse.text
+            test = responseJson["Title"]
+            with open("test.txt", "w") as f:
+                f.write(responseText)
+            return render_template("searchByTitle.html", movieData=test)
+
+@app.route("/movieID", methods=['GET', 'POST'])
+def getID():
+    if request.method == 'POST':
+        if request.form["movieID"] != "":
+            imdbID = request.form["movieID"]
+            requestUrl = "http://www.omdbapi.com/?apikey=a514574c&i=" + imdbID
+            apiResponse = requests.get(requestUrl)
+            responseJson = apiResponse.json()
+            responseText = apiResponse.text
+            with open("test.txt", "w") as f:
+                f.write(responseText)
+            return render_template("searchByID.html", movieData=responseJson)
 
 if __name__ == "__main__":
     app.run(debug=True)
