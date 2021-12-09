6513c2cd81eaefd7052e67f45c460764ad493587
worth-winchester <worthhw@gmail.com>
2021-12-09 18:13:53 +0000

    12/9/21 1:13PM

--- app.py
+++ app.py
@@ -16,42 +16,65 @@ def movieTitle():
 def movieID():
     return render_template("searchByID.html")
 
-# @app.route("/movieTitle", methods=['GET', 'POST'])
-# def getTitle():
-#     if request.method == 'POST':
-#         if request.form["movieTitle"] != "":
-#             title = request.form["movieTitle"]
-#             requestUrl = "http://www.omdbapi.com/?apikey=a514574c&t=" + title
-#             apiResponse = requests.get(requestUrl)
-#             responseJson = apiResponse.json()
-#             responseText = apiResponse.text
-#             with open("test.txt", "w") as f:
-#                 f.write(responseText)
-#             return render_template("searchByTitle.html", movieData=responseJson)
-
 @app.route("/movieTitle", methods=['GET', 'POST'])
 def getTitle():
     if request.method == 'POST':
         if request.form["movieTitle"] != "":
-            title = request.form["movieTitle"]
-            requestUrl = "http://www.omdbapi.com/?apikey=a514574c&t=" + title
+            search = request.form["movieTitle"]
+            requestUrl = "http://www.omdbapi.com/?apikey=a514574c&t=" + search
             apiResponse = requests.get(requestUrl)
             responseJson = apiResponse.json()
             responseText = apiResponse.text
-            test = responseJson["Title"]
+            title = "Title: " + responseJson["Title"]
+            posterUrl = responseJson["Poster"]
+            posterText = "View Movie Poster"
+            year = "Year: " + responseJson["Year"]
+            rated = "Rated: " + responseJson["Rated"]
+            runtime = "Runtime: " + responseJson["Runtime"]
+            genre = "Genre: " + responseJson["Genre"]
+            director = "Director: " + responseJson["Director"]
+            writer = "Writer: " + responseJson["Writer"]
+            actors = "Actors: " + responseJson["Actors"]
+            plot = "Plot: " + responseJson["Plot"]
+            awards = "Awards: " + responseJson["Awards"]
+            metascore = "Metascore: " + responseJson["Metascore"]
+            imdbRating = "IMDB Rating: " + responseJson["imdbRating"]
+            imdbVotes = "IMDB Votes: " + responseJson["imdbVotes"]
+            imdbID = "IMDB ID: " + responseJson["imdbID"]
+            boxOffice = "BoxOffice: " + responseJson["BoxOffice"]
             with open("test.txt", "w") as f:
                 f.write(responseText)
-            return render_template("searchByTitle.html", movieData=test)
+            return render_template("searchByTitle.html", dataTitle=title, dataPosterUrl=posterUrl, dataYear=year, dataRated=rated, dataRuntime=runtime, dataGenre=genre, dataDirector=director, dataWriter=writer, dataActors=actors, dataPlot=plot, dataAwards=awards, dataMetascore=metascore, dataImdbRating=imdbRating, dataImdbVotes=imdbVotes, dataImdbID=imdbID, dataBoxOffice=boxOffice, dataPosterText=posterText)
 
 @app.route("/movieID", methods=['GET', 'POST'])
 def getID():
     if request.method == 'POST':
         if request.form["movieID"] != "":
-            imdbID = request.form["movieID"]
-            requestUrl = "http://www.omdbapi.com/?apikey=a514574c&i=" + imdbID
+            search = request.form["movieID"]
+            requestUrl = "http://www.omdbapi.com/?apikey=a514574c&i=" + search
             apiResponse = requests.get(requestUrl)
             responseJson = apiResponse.json()
             responseText = apiResponse.text
+            title = "Title: " + responseJson["Title"]
+            posterUrl = responseJson["Poster"]
+            posterText = "View Movie Poster"
+            year = "Year: " + responseJson["Year"]
+            rated = "Rated: " + responseJson["Rated"]
+            runtime = "Runtime: " + responseJson["Runtime"]
+            genre = "Genre: " + responseJson["Genre"]
+            director = "Director: " + responseJson["Director"]
+            writer = "Writer: " + responseJson["Writer"]
+            actors = "Actors: " + responseJson["Actors"]
+            plot = "Plot: " + responseJson["Plot"]
+            awards = "Awards: " + responseJson["Awards"]
+            metascore = "Metascore: " + responseJson["Metascore"]
+            imdbRating = "IMDB Rating: " + responseJson["imdbRating"]
+            imdbVotes = "IMDB Votes: " + responseJson["imdbVotes"]
+            imdbID = "IMDB ID: " + responseJson["imdbID"]
+            boxOffice = "BoxOffice: " + responseJson["BoxOffice"]
+            with open("test.txt", "w") as f:
+                f.write(responseText)
+            return render_template("searchByTitle.html", dataTitle=title, dataPosterUrl=posterUrl, dataYear=year, dataRated=rated, dataRuntime=runtime, dataGenre=genre, dataDirector=director, dataWriter=writer, dataActors=actors, dataPlot=plot, dataAwards=awards, dataMetascore=metascore, dataImdbRating=imdbRating, dataImdbVotes=imdbVotes, dataImdbID=imdbID, dataBoxOffice=boxOffice, dataPosterText=posterText)
             with open("test.txt", "w") as f:
                 f.write(responseText)
             return render_template("searchByID.html", movieData=responseJson)
